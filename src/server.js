3b8ee9696bb7a41c8539998ae8d93dcd02f79edd
gintautasm <gintautasm@users.noreply.github.com>
2022-04-18 22:18:07 +0300

    Request buffer (#1)
    * request buffer WIP
    
    * request buffer of variable length
    
    Co-authored-by: gintautasm <gintautasm@noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const events = require('events');
 
+const eventEmitter = new events.EventEmitter();
 const app = express()
 
 app.use(bodyParser.json())
 
-const FluentClient = require("@fluent-org/logger").FluentClient;
-const logger = new FluentClient("cloudwatch.input", {
-    socket: {
-        host: "localhost",
-        port: 24224,
-        timeout: 3000, // 3 seconds
-    }
-});
+// const FluentClient = require("@fluent-org/logger").FluentClient;
+// const logger = new FluentClient("cloudwatch.input", {
+//     socket: {
+//         host: "localhost",
+//         port: 24224,
+//         timeout: 3000, // 3 seconds
+//     }
+// });
 
 const processData = (logEvents) => {
     logEvents.map(logEvent => {
@@ -23,29 +25,62 @@ const processData = (logEvents) => {
 
 }
 
+const EVENT_NAME = 'cloudwatch.input'
+eventEmitter.addListener(EVENT_NAME, (buffer) => {
+    const records = buffer.pop()?.records
+    if (!records) return
 
-app.get('/', function (req, res) {
-    res.setHeader('content-type', 'application/json')
-    res.status(200).send(JSON.stringify({ 'ping': 'Ok' }))
-})
+    records.map(record => {
+        let decodedData = JSON.parse(Buffer.from(record.data, 'base64'))
+        // // firehose test provides ticker
+        // if (JSON.parse(decodedData).SECTOR !== 'ENERGY') return
 
-app.post('/firehose', function (req, res) {
-
-    req.body.records.map(record => {
-        let decodedData = Buffer.from(record.data, 'base64')
+        // // cloudwatch logs can have no data
         // if (record.data.messageType !== 'DATA_MESSAGE') {
         //     //console.log(record.data)
         //     return;
         // }
 
         // processData(record.data.logEvents)
-        logger.emit(decodedData)
+        //logger.emit(decodedData)
+        console.log(JSON.parse(decodedData))
     })
 
-    res.setHeader('content-type', 'application/json')
-    res.status(200).send(JSON.stringify({
-        'requestId': req.body.requestId, timestamp: Math.floor(Date.now())
-    }))
 })
 
-app.listen(process.env['HTTP_SERVER_PORT'])
+
+app.get('/', function (req, res) {
+    res.setHeader('content-type', 'application/json')
+    res.status(200).send(JSON.stringify({ 'ping': 'Ok' }))
+})
+
+const firehoseRqsBuffer = []
+const MAX_BUFFER_SIZE = process.env['MAX_BUFFER_SIZE'] || 5
+
+app.post(`/${EVENT_NAME}`, function (req, res) {
+    if (firehoseRqsBuffer.length < MAX_BUFFER_SIZE) {
+
+        firehoseRqsBuffer.push(req.body)
+
+        res.setHeader('content-type', 'application/json')
+        res.status(200).send(JSON.stringify({
+            'requestId': req.body.requestId,
+            timestamp: Math.floor(Date.now())
+        }))
+
+    } else {
+
+        res.setHeader('content-type', 'application/json')
+        res.status(429).send(JSON.stringify({
+            'requestId': req.body.requestId,
+            timestamp: Math.floor(Date.now()),
+            errorMessage: 'request buffer is full'
+        }))
+    }
+
+    eventEmitter.emit(EVENT_NAME, firehoseRqsBuffer)
+})
+
+const port = process.env['HTTP_SERVER_PORT'] || 6464
+console.log('listening on port:', port)
+app.listen(port)
